7214617bd24559fcf480207e7cf3808d098756dd
phoenixfiretv <dennis.murray10@gmail.com>
2026-02-05 11:37:51 +0000

    Refactor settings for production: update SECRET_KEY format, set DEBUG to True, and adjust ALLOWED_HOSTS. Update requirements.txt to include PostgreSQL packages and add .gitignore for env.py and .github.

--- codestar/settings.py
+++ codestar/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)o62+qaq8j%wk1p$3k8k$tmi5s@5%fj9#83z8w2q!pg$vde=r)'
+SECRET_KEY = (
+    'django-insecure-)o62+qaq8j%wk1p$3k8k$tmi5s@5%fj9#83z8w2q!pg$vde='
+    'r)'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
@@ -87,16 +90,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
