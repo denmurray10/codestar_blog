5947892227177195f9da228a1a9d911d06badf62
phoenixfiretv <dennis.murray10@gmail.com>
2026-02-05 14:50:35 +0000

    Added addtional excerpt to blog form

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,9 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    excerpt = models.TextField(blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
 
